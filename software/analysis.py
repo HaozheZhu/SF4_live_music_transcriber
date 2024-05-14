0e76ac6ff87b1fd7152d93049ba8d6c59766c817
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-14 10:08:22 +0100

    note_frequency_conversion

--- software/analysis.py
+++ software/analysis.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     sample_rate, data = wavfile.read('./software/test.wav') # data has two channels, left and right
     data = data[:, 0]  # Use only one channel
-    start_sec = 0.03
-    end_sec = 0.08
+    start_sec = 0.0
+    end_sec = 1
     time = np.arange(0, float(data.shape[0]), 1) / sample_rate
     # Use only the first few seconds for testing purposes
     time = time[(int(start_sec*sample_rate)):(int(end_sec*sample_rate))] 
@@ -28,13 +28,17 @@ if __name__ == '__main__':
     ax[0].set_title('Audio Signal (time domain)')
 
     # Compute the Fourier Transform
-    fourier = fft.fft(data) / len(data)
     n = len(data)
+    fourier = fft.fft(data) / n
     freq = fft.fftfreq(n, 1/sample_rate)
-    ax[1].plot(freq[:n//2], np.abs(fourier)[:n//2], linewidth=0.6, alpha = 0.9, color='black')
+    spectrum_freq = freq[:n//2]
+    spectrum_mag = np.abs(fourier)[:n//2]
+    ax[1].plot(spectrum_freq, spectrum_mag, linewidth=0.6, alpha = 0.9, color='black')
     ax[1].set_xlabel('Frequency (Hz)')
     ax[1].set_ylabel('Amplitude')
     ax[1].set_title('Audio Signal (frequency domain)')
 
     # TODO: Find the fundamental frequency of the note
+    peaks, _ = signal.find_peaks(spectrum_mag, height=max(spectrum_mag)*0.5, distance=50)
+    ax[1].plot(spectrum_freq[peaks], spectrum_mag[peaks], 'x', color='red')
     plt.show()
