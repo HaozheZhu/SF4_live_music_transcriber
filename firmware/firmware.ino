247dd0c99e5440767eaa9599493304755d024134
WilliaaamWang <william_wang0305@outlook.com>
2024-05-19 19:38:39 +0100

    Added comments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,9 +4,10 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(ANALOG_PIN0, INPUT);
   Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  // Turn-on Turn-off mechanism using mechanical button to begin/end the recording
 }
