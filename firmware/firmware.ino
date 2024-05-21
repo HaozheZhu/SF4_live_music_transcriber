741020ed0b4cfb0b401658b716bfe6baadb8f1f8
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-21 10:27:43 +0100

    change to Serial.write

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,9 +1,14 @@
 #define PI 3.1415926535897932384626433832795
 
-void setup() {
-    Serial.begin(921600);
-    Serial.println("Hello, World!");
+void sendValue(uint16_t value) {
+    Serial.write(value >> 8);
+    Serial.write(value & 0xff);
+}
 
+void setup() {
+    Serial.begin(230400);
+    sendValue(1023);
+    sendValue(512);
 }
 uint32_t period = 100000; // microseconds
 uint32_t samples_per_period = 500;
@@ -13,26 +18,11 @@ uint32_t current_sample_time = 0;
 uint32_t elapsed_time = 0;
 
 void loop() {
-    while (elapsed_time < sample_period) {
-        elapsed_time = micros() - current_sample_time;
-    }
-    current_sample_time = micros();
-    value = 512 * sin(2 * PI * current_sample_time / period) + 512;
-    Serial.println(value);
-    elapsed_time = 0;
-
-
-
-
-
-
-    // uint32_t time = micros(); // microseconds
-    // double angle = 2 * PI * time / period;
-    // uint32_t elapsed_time = micros() - time;
-    // while (elapsed_time < period / samples_per_period) {
-    //     elapsed_time = micros() - time;
+    // while (elapsed_time < sample_period) {
+    //     elapsed_time = micros() - current_sample_time;
     // }
-    // angle = 2 * PI * time / period;
-    // value = 512 * sin(angle) + 512;
-    // Serial.println(value); 
+    // current_sample_time = micros();
+    // value = 512 * sin(2 * PI * current_sample_time / period) + 512;
+    // Serial.write(value);
+    // elapsed_time = 0;
 }
