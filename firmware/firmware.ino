ace6f2ba00996b0107702cc9422a18b2d765db2c
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-24 11:40:31 +0100

    send start command from PC to Arduino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -7,22 +7,26 @@ void sendValue(uint16_t value) {
 
 void setup() {
     Serial.begin(230400);
-    sendValue(1023);
-    sendValue(512);
+    delay(1000);
+    while(Serial.read() != 'S'); 
+    sendValue(0xFFF2);
 }
-uint32_t period = 100000; // microseconds
-uint32_t samples_per_period = 100;
+uint32_t period = 1000000; // microseconds
+uint32_t samples_per_period = 4000;
 uint32_t sample_period = period / samples_per_period;
 uint16_t value; 
-uint32_t current_sample_time = 0;  
+uint32_t current_sample_time = 0; 
 uint32_t elapsed_time = 0;
 
 void loop() {
-    while (elapsed_time < sample_period) {
-        elapsed_time = micros() - current_sample_time;
+    for (int i = 0; i < samples_per_period; i++) {
+        while (elapsed_time < sample_period) {
+            elapsed_time = micros() - current_sample_time;
+        }
+        current_sample_time = micros();
+        value = 512 * sin(2 * PI * current_sample_time / period) + 512;
+        sendValue(value);
+        elapsed_time = 0;
     }
-    current_sample_time = micros();
-    value = 512 * sin(2 * PI * current_sample_time / period) + 512;
-    sendValue(value);
-    elapsed_time = 0;
+    while(1); 
 }
--- software/pySerial.py
+++ software/pySerial.py
@@ -1,18 +1,23 @@
 import serial
+import time
 
 if __name__ == '__main__':
     ser = serial.Serial()
     ser.baudrate = 230400
-    ser.port = '/dev/ttyACM0'
+    ser.port = '/dev/ttyACM2'
     print(ser)         # check which port was really used
     
     ser.open()
     print(ser.is_open) # True for opened port
 
-    data = 0 
+    time.sleep(5)
+    ser.write('S'.encode())
+    print('Start') # Start confirmation received from Arduino
+
     while True:
         data = ser.read(2)
         datalist = list(data)
         data = (datalist[0] << 8) + datalist[1]
+        print(time.time(), end=" ")
         print(data)
     
