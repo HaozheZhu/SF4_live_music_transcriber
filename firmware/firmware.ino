828119f12d998f17aea710b97000715aff52dba5
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-24 12:53:58 +0100

    reliability solved

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -8,11 +8,16 @@ void sendValue(uint16_t value) {
 void setup() {
     Serial.begin(230400);
     delay(1000);
-    while(Serial.read() != 'S'); 
+    uint8_t buf[1];
+    Serial.readBytes(buf, 1); 
+    while(buf[0] != 0x53) {
+        Serial.readBytes(buf, 1); 
+    }
+    delay(500); 
     sendValue(0xFFF2);
 }
 uint32_t period = 1000000; // microseconds
-uint32_t samples_per_period = 4000;
+uint32_t samples_per_period = 100;
 uint32_t sample_period = period / samples_per_period;
 uint16_t value; 
 uint32_t current_sample_time = 0; 
--- software/pySerial.py
+++ software/pySerial.py
@@ -1,10 +1,20 @@
 import serial
 import time
 
+def read_data(ser):
+    data = ser.read(2)
+    datalist = list(data)
+    data = (datalist[0] << 8) + datalist[1]
+    return data
+
+def print_data(data):
+    print(time.time(), end=" ")
+    print(data)
+
 if __name__ == '__main__':
     ser = serial.Serial()
     ser.baudrate = 230400
-    ser.port = '/dev/ttyACM2'
+    ser.port = '/dev/ttyACM4'
     print(ser)         # check which port was really used
     
     ser.open()
@@ -12,12 +22,15 @@ if __name__ == '__main__':
 
     time.sleep(5)
     ser.write('S'.encode())
-    print('Start') # Start confirmation received from Arduino
+    time.sleep(0.1)
+    print("Starting")
+    data = read_data(ser)
+    while data != 0xFFF2:
+        print_data(data)
+        data = read_data(ser)
+    print('Started') # Start confirmation received from Arduino
 
     while True:
-        data = ser.read(2)
-        datalist = list(data)
-        data = (datalist[0] << 8) + datalist[1]
-        print(time.time(), end=" ")
-        print(data)
+        data = read_data(ser)
+        print_data(data)
     
