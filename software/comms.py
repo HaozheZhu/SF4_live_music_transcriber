516d941be2e7cb53006c8989f9fd64f1ff924156
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-25 21:09:21 +0100

    Squashed commit of the following:
    commit 1f558231c29e479f1cd6ed81478d7cbe22f12951
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 21:08:24 2024 +0100
    
        small changes
    
    commit 8475d9874f23192e24b0815ce9db53ba704765b6
    Author: Ruoyao Wang <william_wang0305@outlook.com>
    Date:   Sat May 25 19:42:11 2024 +0100
    
        Changed to perf_counter and produced test data
    
    commit f5be497ca61585dfcc88eb097f63ab17595c89ce
    Author: Ruoyao Wang <william_wang0305@outlook.com>
    Date:   Sat May 25 17:33:10 2024 +0100
    
        Added port for windows
    
    commit 1736373266aa0368421dc24f7396f3e4302b7de7
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 17:26:34 2024 +0100
    
        small changes
    
    commit 187cf7b46bf60f3d15c1412449dc9366cff1b329
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 16:54:12 2024 +0100
    
        small changes
    
    commit 0e55560d6ad3258ca7f1a73e3fd6b82d5ffa2b89
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 16:38:38 2024 +0100
    
        updated gitignore
    
    commit d332dc1beef1b0519b25d79111237738aba5937e
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 16:38:37 2024 +0100
    
        updated gitignore
    
    commit e2f6bf128f9bb27b301019df145f4f09e1a7768c
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 13:45:02 2024 +0100
    
        draw received signal in pySerial module, 4 kHz sampling rate working
    
    commit 185280e08836a30fa9f1aadc3c8980996482d21a
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 11:44:55 2024 +0100
    
        print collected data summary
    
    commit 64b58599d43a7edea770b655f02cb49b7b241060
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Sat May 25 11:11:00 2024 +0100
    
        extracted Arduino_reset into a function
    
    commit aa00f01ad3b13589f5f377c2e46abe6b3ef112cf
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 22:07:43 2024 +0100
    
        extracted timer into a function
    
    commit 2d3d951d40b1f7169dfa1edde0d19f83c62eb2a0
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:52:26 2024 +0100
    
        updated commands table
    
    commit f04f967d43b926bc6fc0b307dfff7655b6f24083
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:48:33 2024 +0100
    
        add Arduino ready signal to remind resetting Arduino
    
    commit 022946a00836bfe709d042d5f2275e221b4197e0
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:34:20 2024 +0100
    
        added Arduino LED (pin 13) to indicate Arduino status
    
    commit a6ad6326f29e1af71aac799b13663ad5a80d559d
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 13:47:39 2024 +0100
    
        Start on user input
    
    commit 1e5bd31494d13cc607911b78477f9e633e893d9d
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 13:42:45 2024 +0100
    
        extracted starting routine into functions
    
    commit 828119f12d998f17aea710b97000715aff52dba5
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 12:53:58 2024 +0100
    
        reliability solved
    
    commit ace6f2ba00996b0107702cc9422a18b2d765db2c
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 11:40:31 2024 +0100
    
        send start command from PC to Arduino

--- software/comms.py
+++ software/comms.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 def serial_setup(): 
     ser = serial.Serial()
     ser.baudrate = 230400
+    # For Linux
     ser.port = '/dev/ttyACM0'
+    # For Windows
+    # ser.port = 'COM4'
     ser.open()
     if ser.is_open:
         print("Serial setup complete")
@@ -51,16 +54,16 @@ if __name__ == '__main__':
 
     try: 
         data_list = []
-        start_time = time.time()
+        start_time = time.perf_counter()
         while True:
             data = read_data(ser)
-            data_list.append((time.time()-start_time, data))
+            data_list.append((time.perf_counter()-start_time, data))
     except KeyboardInterrupt:
         print("Exiting...")
         ser.close()
         print("=====================================")
         print("Number of data points: ", len(data_list))
-        print("Time elapsed: ", time.time()-start_time)
+        print("Time elapsed: ", time.perf_counter()-start_time)
         print("Sample rate: ", len(data_list)/data_list[-1][0])
         df = pd.DataFrame(data_list, columns=['Time', 'Data'])
         df.to_csv('./software/tmp/data.csv', index=False)
