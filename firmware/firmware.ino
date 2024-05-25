3a23e4769962f057b6b24be140e56db8dab841c3
Haozhe Zhu <hz403@cam.ac.uk>
2024-05-25 10:58:47 +0100

    PC send start command to Arduino working, reliability problem solved
    Squashed commit of the following:
    
    commit aa00f01ad3b13589f5f377c2e46abe6b3ef112cf
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 22:07:43 2024 +0100
    
        extracted timer into a function
    
    commit 2d3d951d40b1f7169dfa1edde0d19f83c62eb2a0
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:52:26 2024 +0100
    
        updated commands table
    
    commit f04f967d43b926bc6fc0b307dfff7655b6f24083
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:48:33 2024 +0100
    
        add Arduino ready signal to remind resetting Arduino
    
    commit 022946a00836bfe709d042d5f2275e221b4197e0
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 21:34:20 2024 +0100
    
        added Arduino LED (pin 13) to indicate Arduino status
    
    commit a6ad6326f29e1af71aac799b13663ad5a80d559d
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 13:47:39 2024 +0100
    
        Start on user input
    
    commit 1e5bd31494d13cc607911b78477f9e633e893d9d
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 13:42:45 2024 +0100
    
        extracted starting routine into functions
    
    commit 828119f12d998f17aea710b97000715aff52dba5
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 12:53:58 2024 +0100
    
        reliability solved
    
    commit ace6f2ba00996b0107702cc9422a18b2d765db2c
    Author: Haozhe Zhu <hz403@cam.ac.uk>
    Date:   Fri May 24 11:40:31 2024 +0100
    
        send start command from PC to Arduino

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,33 +1,57 @@
 #define PI 3.1415926535897932384626433832795
 
+uint32_t period = 1000000; // microseconds
+uint32_t samples_per_period = 1000;
+uint32_t sample_period = period / samples_per_period;
+uint32_t current_sample_time = 0; 
+uint32_t elapsed_time = 0;
+
 void sendValue(uint16_t value) {
     Serial.write(value >> 8);
     Serial.write(value & 0xff);
 }
 
+void starting_routine() {
+    uint8_t buf[1];
+    Serial.readBytes(buf, 1); 
+    while(buf[0] != 0x53) {
+        Serial.readBytes(buf, 1); 
+    }
+    delay(500); 
+    sendValue(0xFFF2);
+    digitalWrite(13, LOW); // turn off LED to indicate that Arduino has started
+}
+
+uint16_t dummy_data_generation() {
+    return 512 * sin(2 * PI * current_sample_time / period) + 512;
+}
+
+uint16_t sample_ADC() {
+    return analogRead(A0);
+}
+
+// runs the function pointed by funcptr repeatedly
+void timer_routine(uint16_t(*funcptr)()) {
+    while (elapsed_time < sample_period) {
+        elapsed_time = micros() - current_sample_time;
+    }
+    current_sample_time = micros();
+    uint16_t value = funcptr();
+    sendValue(value);
+    elapsed_time = 0;
+}
+
 void setup() {
     Serial.begin(230400);
-    // sendValue(1023);
-    // sendValue(512);
-
-    for (int i = 0; i <500; i++) {
-        sendValue(1023);
-        sendValue(512);
-    }
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH); // turn on LED to indicate that Arduino is ready to start
+    Serial.write('R'); // send ready signal to PC
+    starting_routine();
 }
-uint32_t period = 100000; // microseconds
-uint32_t samples_per_period = 500;
-uint32_t sample_period = period / samples_per_period;
-uint16_t value; 
-uint32_t current_sample_time = 0;  
-uint32_t elapsed_time = 0;
 
 void loop() {
-    // while (elapsed_time < sample_period) {
-    //     elapsed_time = micros() - current_sample_time;
-    // }
-    // current_sample_time = micros();
-    // value = 512 * sin(2 * PI * current_sample_time / period) + 512;
-    // sendValue(value);
-    // elapsed_time = 0;
+    for (int i = 0; i < samples_per_period; i++) {
+        timer_routine(dummy_data_generation);
+    }
+    while(1); 
 }
--- software/pySerial.py
+++ software/pySerial.py
@@ -1,7 +1,7 @@
 import serial
-from time import perf_counter
+import time
 
-if __name__ == '__main__':
+def serial_setup(): 
     ser = serial.Serial()
     ser.baudrate = 230400
     ser.port = '/dev/ttyACM0'
@@ -10,11 +10,39 @@ if __name__ == '__main__':
     
     ser.open()
     print(ser.is_open) # True for opened port
+    return ser
 
-    data = 0 
+def read_data(ser):
+    data = ser.read(2)
+    datalist = list(data)
+    data = (datalist[0] << 8) + datalist[1]
+    return data
+
+def print_data(data):
+    print(time.time(), end=" ")
+    print(data)
+
+def starting_routine(ser): 
+    while input("Input 's' to start: ") not in ['S', 's']:
+        pass
+    ser.write('S'.encode())
+    time.sleep(0.1)
+    print("Starting")
+    data = read_data(ser)
+    while data != 0xFFF2:
+        print_data(data)
+        data = read_data(ser)
+    print('Started') # Start confirmation received from Arduino
+
+if __name__ == '__main__':
+    ser = serial_setup()
+    print("Please reset the Arduino (sometimes it is done automatically)")
+    while ser.read(1) != b'R':
+        pass
+    print("Arduino ready")
+    starting_routine(ser)
+
     while True:
-        data = ser.read(2)
-        datalist = list(data)
-        data = (datalist[0] << 8) + datalist[1]
-        print(f'{perf_counter()}, \t{data}')
+        data = read_data(ser)
+        print_data(data)
     
